9980a40c8b380b6c928ea8153753661f70ed14fc
Unique <uniquechhetrii@gmail.com>
2018-05-22 20:32:05 +0545

    Session

--- Unique Demo/model/schema.js
+++ Unique Demo/model/schema.js
@@ -19,7 +19,29 @@ var LoginSchema=new Schema({
 	}
 });
 
-var Login=mongoose.model('logindetails',LoginSchema);
+//authentication 
+LoginSchema.statics.authenticate=function (password,callback){
+	Login.findOne({username: username})
+	.exec(function(err,login){
+		if(err){
+			return callback(err)
+		}
+		else if(!login){
+			var err=new Error('User not found.');
+			err.status=401;
+			return callback(err);
+		}
+		 bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+	}
+	})
+});
+}
+
+
 
 //Hashing the password
 LoginSchema.pre('save', function (next) {
@@ -41,4 +63,5 @@ LoginSchema.pre('save', function (next) {
 });
 
 
+var Login=mongoose.model('logindetails',LoginSchema);
 module.exports=Login;
--- Unique Demo/server.js
+++ Unique Demo/server.js
@@ -1,12 +1,26 @@
 var express=require('express');
 var bodyParser=require('body-parser');
 var routes=require('./routes');
+var session=require('express-session');
 
 var app=express();
+
+//connection to database
 var mongoose=require('mongoose');
 mongoose.connect('mongodB://localhost/Details');
 mongoose.Promise=global.Promise;
+
+//handling session
+app.use(session({
+	secret: 'work hard',
+	resave: true,
+	saveUninitialized: false
+}));
+
+//body parser
 app.use(bodyParser.json());
+
+//route handling
 app.use(routes);
 
 
